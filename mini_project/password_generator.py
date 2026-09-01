03944d1a3a2b52fa94b13e7676d49f1406fc15cc
s3944714 <s3944714@student.rmit.edu.au>
2026-09-01 14:38:24 +1000

    define function and easy and hard levels

--- mini_project/password_generator.py
+++ mini_project/password_generator.py
@@ -1,15 +1,27 @@
 import string
 import random
 
-all_letters_list = list(string.ascii_letters)
-symbols_list = list(string.punctuation)
-combined = all_letters_list + symbols_list
+easy_chars = list(string.ascii_lowercase + string.digits)
+hard_chars = list(string.ascii_letters + string.punctuation) 
 
 
 
 num = int(input("How much characters do you want to pick for your password: "))
-generate = random.choices(combined,k=num)
-word = ''.join(generate)
-print(word)
+user_level= input("Enter User level: ").lower()
 
 
+def generate_password(length: int, level: str) -> str:
+    if length <= 0:
+        return "invalid length"
+
+    if level == "easy":
+        generate = random.choices(easy_chars,k=num)
+        word = ''.join(generate)
+        return word
+    if level == "hard":
+        generate = random.choices(hard_chars,k=num)
+        word = ''.join(generate)
+        return word
+
+password = generate_password(num, user_level)
+print(password)
