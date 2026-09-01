403181dfcd6ad905f92496d2e3a3da05ba0eca81
s3944714 <s3944714@student.rmit.edu.au>
2026-09-01 13:03:46 +1000

    mini project for password generator

---
+++ mini_project/password_generator.py
@@ -0,0 +1,14 @@
+import string
+import random
+
+all_letters_list = list(string.ascii_letters)
+symbols_list = list(string.punctuation)
+combined = all_letters_list + symbols_list
+
+
+num = int(input("How much characters do you want to pick for your password: "))
+generate = random.sample(combined,num)
+word = ''.join(generate)
+print(word)
+
+
