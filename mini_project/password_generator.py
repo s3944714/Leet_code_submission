5b8c04f6a0d366046391b5821e1a573d47f13d01
s3944714 <s3944714@student.rmit.edu.au>
2026-09-01 13:24:38 +1000

    change  choice to choices to include k=num

--- mini_project/password_generator.py
+++ mini_project/password_generator.py
@@ -6,8 +6,9 @@ symbols_list = list(string.punctuation)
 combined = all_letters_list + symbols_list
 
 
+
 num = int(input("How much characters do you want to pick for your password: "))
-generate = random.sample(combined,num)
+generate = random.choices(combined,k=num)
 word = ''.join(generate)
 print(word)
 
