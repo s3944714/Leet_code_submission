7f3640a622460288254a4507f994313298ff2e95
s3944714 <s3944714@student.rmit.edu.au>
2026-09-04 13:00:57 +1000

    mini exercise on dictionaries

---
+++ exercise/grade_calculator.py
@@ -0,0 +1,21 @@
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+student_grades = {}
+for student, score in student_scores.items():
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif score > 80:
+        student_grades[student] = "Exceeds Expectations"
+    elif score > 70:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+
+
+
+
